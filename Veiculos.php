30e3fae178b8db548e90ec7d3fe335f1a3ec057c
oscauebarbosa <124931002+oscauebarbosa@users.noreply.github.com>
2023-03-09 16:16:08 -0300

    Update Veiculos.php

--- Veiculos.php
+++ Veiculos.php
@@ -1,13 +1,26 @@
 <?php
+class Carro {
+    public $placa;
+    public $marca;
+    public $ipva;
+    public $ano;
 
-$placa = "YAG4321";
-$marca = "Saveiro CS";
-$ipva = 600.00;
-$ano = 2012;
+    public function __construct($placa, $marca, $ipva, $ano) {
+        $this->placa = $placa;
+        $this->marca = $marca;
+        $this->ipva = $ipva;
+        $this->ano = $ano;
+    }
 
-echo "Placa: " . $placa . "<br>";
-echo "Marca: " . $marca . "<br>";
-echo "IPVA: R$ " . number_format($ipva, 2, ",", ".") . "<br>";
-echo "Ano: " . $ano . "<br>";
+    public function exibirInformacoes() {
+        echo "Placa: " . $this->placa . "<br>";
+        echo "Marca: " . $this->marca . "<br>";
+        echo "IPVA: R$ " . number_format($this->ipva, 2, ",", ".") . "<br>";
+        echo "Ano: " . $this->ano . "<br>";
+    }
+}
+
+$carro = new Carro("YAG4321", "Saveiro CS", 600.00, 2012);
+$carro->exibirInformacoes();
 
 ?>
